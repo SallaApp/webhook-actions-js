b5211506469508ea46e6e27cf82da9950f0952e8
ajibadeabd <43596899+ajibadeabd@users.noreply.github.com>
2024-08-13 10:45:16 +0100

    updated test sample data for event

--- example/Actions/order/created.js
+++ example/Actions/order/created.js
@@ -1,26 +1,229 @@
 /**
- *  this function is exeucted on "order.created" action triggered by Salla .
+ *  This function is executed on the "order.created" action triggered by Salla.
  *
  * Action Body received from Salla
  * @param {Object} eventBody
  * { 
  *  event: 'order.created',
-    merchant: 472944967,
-    created_at: '2021-11-22 13:51:57',
-    data:
+ *  merchant: 1305146709,
+ *  created_at: 'Sun Jun 26 2022 12:21:48 GMT+0300',
+ *  data:
  *    {
- *      "id":1911645512,
- *      "app_name":"app name",
- *      "app_description":"desc",
- *      "app_type":"app",
- *      "app_scopes":[ 
- *        'settings.read',
- *        'customers.read_write',
- *        'orders.read_write',
- *        'carts.read',
- *        ...
+ *      "id": 2116149737,
+ *      "reference_id": 41027662,
+ *      "urls": {
+ *        "customer": "https://salla.sa/dev-wofftr4xsra5xtlv/order/DXZbOz68qjnYaOw04oBa35wVELBpJyxo",
+ *        "admin": "https://s.salla.sa/orders/order/DXZbOz68qjnYaOw04oBa35wVELBpJyxo"
+ *      },
+ *      "date": {
+ *        "date": "2022-06-26 12:21:45.000000",
+ *        "timezone_type": 3,
+ *        "timezone": "Asia/Riyadh"
+ *      },
+ *      "draft": false,
+ *      "read": true,
+ *      "source": "store",
+ *      "source_device": "desktop",
+ *      "source_details": {
+ *        "type": "direct",
+ *        "value": null,
+ *        "device": "desktop",
+ *        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36",
+ *        "ip": "31.166.186.78"
+ *      },
+ *      "status": {
+ *        "id": 566146469,
+ *        "name": "بإنتظار المراجعة",
+ *        "slug": "under_review",
+ *        "customized": {
+ *          "id": 986688842,
+ *          "name": "بإنتظار المراجعة"
+ *        }
+ *      },
+ *      "receipt_image": "https://cdn.salla.sa/jKxK/md4Pv5jeLEX52zWTboKSVr8VmlX5tNbiu9YF00C0.png",
+ *      "payment_method": "bank",
+ *      "currency": "SAR",
+ *      "amounts": {
+ *        "sub_total": {
+ *          "amount": 186,
+ *          "currency": "SAR"
+ *        },
+ *        "shipping_cost": {
+ *          "amount": 15,
+ *          "currency": "SAR"
+ *        },
+ *        "cash_on_delivery": {
+ *          "amount": 0,
+ *          "currency": "SAR"
+ *        },
+ *        "tax": {
+ *          "percent": "0.00",
+ *          "amount": {
+ *            "amount": 0,
+ *            "currency": "SAR"
+ *          }
+ *        },
+ *        "discounts": [
+ *          {
+ *            "title": "new offer",
+ *            "type": "special",
+ *            "code": "new offer",
+ *            "discount": "5.00",
+ *            "discounted_shipping": 0
+ *          }
+ *        ],
+ *        "total": {
+ *          "amount": 196,
+ *          "currency": "SAR"
+ *        }
+ *      },
+ *      "shipping": {
+ *        "id": 1833934431,
+ *        "company": "السعودية",
+ *        "receiver": {
+ *          "name": "Mohammed Ali",
+ *          "email": "usertest@gmail.com",
+ *          "phone": "999123456789"
+ *        },
+ *        "shipper": {
+ *          "name": "Demo",
+ *          "company_name": "dev-wofftr4xsra5xtlv",
+ *          "email": "wofftr4xsra5xtlv@email.partners",
+ *          "phone": "966500000000"
+ *        },
+ *        "pickup_address": {
+ *          "country": "السعودية",
+ *          "city": "مكة",
+ *          "shipping_address": "شارع عبدالله,السلام,23233, سنابل السلام, مكة,السعودية",
+ *          "street_number": "شارع عبدالله",
+ *          "block": "السلام",
+ *          "postal_code": "23233",
+ *          "geo_coordinates": {
+ *            "lat": 21.3825905096851,
+ *            "lng": 39.77319103068542
+ *          }
+ *        },
+ *        "address": {
+ *          "country": "SA",
+ *          "city": "جدة",
+ *          "shipping_address": " شارع ابو امية الضمري، الحي الزهراء ،, جدة, السعودية",
+ *          "street_number": "ابو امية الضمري",
+ *          "block": "الزهراء",
+ *          "postal_code": "",
+ *          "geo_coordinates": {
+ *            "lat": 0,
+ *            "lng": 0
+ *          }
+ *        }
+ *      },
+ *      "items": [
+ *        {
+ *          "id": 70815337,
+ *          "name": "بيتزا",
+ *          "sku": "54534534",
+ *          "quantity": 1,
+ *          "currency": "SAR",
+ *          "weight": 0.25,
+ *          "amounts": {
+ *            "price_without_tax": {
+ *              "amount": 186,
+ *              "currency": "SAR"
+ *            },
+ *            "total_discount": {
+ *              "amount": 5,
+ *              "currency": "SAR"
+ *            },
+ *            "total": {
+ *              "amount": 186,
+ *              "currency": "SAR"
+ *            }
+ *          },
+ *          "product": {
+ *            "id": 720881993,
+ *            "type": "food",
+ *            "promotion": {
+ *              "title": "اطلبها ساخنه",
+ *              "sub_title": "بيتزا خضار مشكل"
+ *            },
+ *            "status": "sale",
+ *            "is_available": true,
+ *            "sku": "54534534",
+ *            "name": "بيتزا",
+ *            "price": {
+ *              "amount": 66,
+ *              "currency": "SAR"
+ *            },
+ *            "sale_price": {
+ *              "amount": 45,
+ *              "currency": "SAR"
+ *            },
+ *            "currency": "SAR",
+ *            "url": "https://salla.sa/dev-wofftr4xsra5xtlv/بيتزا/p720881993",
+ *            "thumbnail": "https://cdn.salla.sa/bYQEn/buItWZf4OLbaTmL7vTMlDUWLOn20hfpq3QUbD2AB.jpg"
+ *          },
+ *          "options": [
+ *            {
+ *              "id": 1197801866,
+ *              "product_option_id": 60176141,
+ *              "name": "SIZE",
+ *              "type": "checkbox",
+ *              "value": [
+ *                {
+ *                  "id": 408420634,
+ *                  "name": "BIG",
+ *                  "price": {
+ *                    "amount": 120,
+ *                    "currency": "SAR"
+ *                  }
+ *                }
+ *              ]
+ *            },
+ *            {
+ *              "id": 289546379,
+ *              "product_option_id": 1674915438,
+ *              "name": "الاضافات",
+ *              "type": "checkbox",
+ *              "value": [
+ *                {
+ *                  "id": 152115913,
+ *                  "name": "بصل",
+ *                  "price": {
+ *                    "amount": 0,
+ *                    "currency": "SAR"
+ *                  }
+ *                },
+ *                {
+ *                  "id": 1526610378,
+ *                  "name": "فلفل",
+ *                  "price": {
+ *                    "amount": 0,
+ *                    "currency": "SAR"
+ *                  }
+ *                }
+ *              ]
+ *            }
+ *          ]
+ *        }
  *      ],
- *      "installation_date":"2021-11-21 11:07:13"
+ *      "customer": {
+ *        "id": 225167971,
+ *        "first_name": "Mohammed",
+ *        "last_name": "Ali",
+ *        "mobile": 501806978,
+ *        "mobile_code": "+966",
+ *        "email": "usertest@gmail.com",
+ *        "avatar": "https://cdn.salla.sa/customer_profiles/5i6SUhu9dlF1fvL3EcV98U644eOlG9jcEipz6dOo.jpg",
+ *        "gender": "female",
+ *        "birthday": {
+ *          "date": "1997-06-03 00:00:00.000000",
+ *          "timezone_type": 3,
+ *          "timezone": "Asia/Riyadh"
+ *        },
+ *        "city": "جدة",
+ *        "country": "السعودية",
+ *        "country_code": "SA",
+ *        "currency": "SAR"
+ *      }
  *    }
  * }
  * Arguments passed by you:
--- example/Actions/project/created.js
+++ example/Actions/project/created.js
@@ -1,31 +1,153 @@
 /**
- *  this function is exeucted on "project.created" action triggered by Salla .
+ * This function is executed on the "product.created" action triggered by Salla.
  *
- * Action Body received from Salla
+ * Action Body received from Salla:
  * @param {Object} eventBody
  * { 
- *  event: 'project.created',
-    merchant: 472944967,
-    created_at: '2021-11-22 13:51:57',
-    data:
- *    {
- *      "id":1911645512,
- *      "app_name":"app name",
- *      "app_description":"desc",
- *      "app_type":"app",
- *      "app_scopes":[ 
- *        'settings.read',
- *        'customers.read_write',
- *        'orders.read_write',
- *        'carts.read',
- *        ...
- *      ],
- *      "installation_date":"2021-11-21 11:07:13"
- *    }
+ *   event: 'product.created',
+ *   merchant: 1305146709,
+ *   created_at: 'Mon Apr 17 2023 12:14:21 GMT+0300',
+ *   data: {
+ *     id: 1025569331,
+ *     promotion: {
+ *       title: null,
+ *       sub_title: null
+ *     },
+ *     sku: '',
+ *     mpn: null,
+ *     gtin: null,
+ *     type: 'booking',
+ *     name: 'Product-Booking-API-Date-and-Time',
+ *     short_link_code: 'aezvrRn',
+ *     urls: {
+ *       customer: 'https://salla.sa/dev-wofftr4xsra5xtlv/product-booking-api-date-and-time/p1025569331',
+ *       admin: 'https://s.salla.sa/products/1025569331'
+ *     },
+ *     price: {
+ *       amount: 299,
+ *       currency: 'SAR'
+ *     },
+ *     taxed_price: {
+ *       amount: 343.85,
+ *       currency: 'SAR'
+ *     },
+ *     pre_tax_price: {
+ *       amount: 299,
+ *       currency: 'SAR'
+ *     },
+ *     tax: {
+ *       amount: 44.85,
+ *       currency: 'SAR'
+ *     },
+ *     description: '',
+ *     quantity: 20,
+ *     status: 'hidden',
+ *     is_available: false,
+ *     views: 0,
+ *     sale_price: {
+ *       amount: 0,
+ *       currency: 'SAR'
+ *     },
+ *     sale_end: null,
+ *     require_shipping: false,
+ *     cost_price: null,
+ *     weight: 0,
+ *     weight_type: null,
+ *     with_tax: true,
+ *     url: 'https://salla.sa/dev-wofftr4xsra5xtlv/product-booking-api-date-and-time/p1025569331',
+ *     main_image: null,
+ *     images: [],
+ *     sold_quantity: 0,
+ *     rating: {
+ *       total: 0,
+ *       count: 0,
+ *       rate: 0
+ *     },
+ *     regular_price: {
+ *       amount: 299,
+ *       currency: 'SAR'
+ *     },
+ *     max_items_per_user: 1,
+ *     maximum_quantity_per_order: null,
+ *     show_in_app: false,
+ *     notify_quantity: null,
+ *     hide_quantity: true,
+ *     unlimited_quantity: false,
+ *     managed_by_branches: false,
+ *     services_blocks: {
+ *       installments: []
+ *     },
+ *     calories: null,
+ *     customized_sku_quantity: false,
+ *     channels: [],
+ *     starting_price: null,
+ *     metadata: {
+ *       title: null,
+ *       description: null
+ *     },
+ *     booking_details: {
+ *       id: 231783151,
+ *       type: 'date_time',
+ *       session_duration: 50,
+ *       session_gap: 3,
+ *       sessions_count: 10,
+ *       location: 'Medina',
+ *       time_strict_type: 'days',
+ *       time_strict: 5,
+ *       overrides: [
+ *         {
+ *           id: 1476756183,
+ *           date: '2023-04-23'
+ *         },
+ *         {
+ *           id: 904045008,
+ *           date: '2023-04-24'
+ *         }
+ *       ],
+ *       availabilities: [
+ *         {
+ *           day: 'sunday',
+ *           is_available: true,
+ *           times: [
+ *             {
+ *               from: '09:30',
+ *               to: '13:00'
+ *             },
+ *             {
+ *               from: '12:00',
+ *               to: '22:00'
+ *             }
+ *           ]
+ *         },
+ *         {
+ *           day: 'monday',
+ *           is_available: true,
+ *           times: [
+ *             {
+ *               from: '10:00',
+ *               to: '12:00'
+ *             }
+ *           ]
+ *         }
+ *       ]
+ *     },
+ *     allow_attachments: false,
+ *     is_pinned: false,
+ *     pinned_date: '2023-04-17 12:14:20',
+ *     sort: 0,
+ *     enable_upload_image: false,
+ *     updated_at: '2023-04-17 12:14:21',
+ *     options: [],
+ *     skus: [],
+ *     categories: [],
+ *     brand: null,
+ *     tags: []
+ *   }
  * }
+ *
  * Arguments passed by you:
  * @param {Object} userArgs
- * { key:"val" }
+ * { key: 'val' }
  * @api public
  */
 module.exports = (eventBody, userArgs) => {
